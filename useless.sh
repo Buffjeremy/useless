c774edd4fb3d7513bcb66145cf8900957a9914ee
Jeremy Schantz <buffjeremy@Jeremys-MacBook-Pro.local>
2018-04-15 23:32:45 -0400

    useless

---
+++ useless.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo welcome $USER
+echo -e 'this program makes it so this program can not be run' 
+sleep 2
+ls -l useless.sh 
+chmod 66 useless.sh
+echo 'it is done' 
+ls -l useless.sh 
+
